01dd20492a897ee0947b4a11a3a7e09187c2706b
MushroomLT <kasparas6@hotmail.com>
2017-06-16 02:04:21 +0300

    Attempt to fix libmtk_symbols #3

--- mtk/libmtk_symbols/mtk_ui.cpp
+++ mtk/libmtk_symbols/mtk_ui.cpp
@@ -14,9 +14,9 @@ extern "C" {
     //    _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(handle, static_cast<uint32_t>(usage), bounds, vaddr);
     //}
 
-    void _ZN7android5Fence4waitEi(int);
+    //void _ZN7android5Fence4waitEi(int);
 
-    void _ZN7android5Fence4waitEj(unsigned int timeout) {
-        _ZN7android5Fence4waitEi(static_cast<int>(timeout));
-    }
+    //void _ZN7android5Fence4waitEj(unsigned int timeout) {
+    //    _ZN7android5Fence4waitEi(static_cast<int>(timeout));
+    //}
 }
